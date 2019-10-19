6dffe1a2ca39666b10c23bc6a84943e4661c8523
yohanes_prima <jooprima@gmail.com>
2019-10-19 22:39:02 +0700

    tugas 4 golang
    tugas ini dikumpulkan untuk NIOMIC

---
+++ tugas4.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	var tinggi_badan = map[string]int{"aldo": 182, "yosep": 178}
+	fmt.Println("Aldo :", tinggi_badan["aldo"], "cm")
+	fmt.Println("Yosep :", tinggi_badan["yosep"], "cm")
+}
